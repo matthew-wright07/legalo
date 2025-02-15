ed4ee1b10c7851468b68e5eb12786c64d135c345
Matthew <matthewjwright07@gmail.com>
2025-02-14 18:14:46 -0600

    Changed Pricing

--- components/pricing/PricingTable.js
+++ components/pricing/PricingTable.js
@@ -4,7 +4,7 @@ export default function PricingTable(){
         <h1 className="text-center text-3xl font-bold text-primary">Pricing</h1>
         <p className="w-1/3 text-center self-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie facilisis velit. Duis non tellus blandit, auctor elit sit amet, pretium odio. Sed rhoncus, orci vel tincidunt congue, dui nulla ultrices velit, at ultricies ex est eu nunc. Nunc in dictum dolor, quis porttitor odio. In id lacus turpis.</p>
     <div className="grid grid-cols-3 gap-4 px-24 p-4 place-items-center h-[40vh]">
-    <div className="bg-background w-2/3 h-full rounded-lg p-4 flex flex-col justify-center items-center gap-4">
+    <div className="border border-primary w-2/3 h-full rounded-lg p-4 flex flex-col justify-center items-center gap-4">
         <h1 className="text-3xl font-bold justify-self-center">Free</h1>
         <h1 className="text-4xl font-bold text-primary">$0<span className="text-sm text-black font-normal">/month</span></h1>
         <ul className="text-center list-disc">
@@ -14,17 +14,17 @@ export default function PricingTable(){
         </ul>
         <button className="w-1/2 bg-primary text-white rounded-lg h-10">Click Here</button>
     </div>
-    <div className="bg-background w-2/3 h-full rounded-lg p-4 flex flex-col justify-center items-center gap-4">
+    <div className="bg-primary w-2/3 h-full rounded-lg p-4 flex flex-col justify-center items-center gap-4 text-white">
         <h1 className="text-3xl font-bold justify-self-center">Monthly</h1>
-        <h1 className="text-4xl font-bold text-primary">$8<span className="text-sm text-black font-normal">/month</span></h1>
+        <h1 className="text-4xl font-bold">$8<span className="text-sm font-normal">/month</span></h1>
         <ul className="text-center list-disc">
             <li>Unlimited Document Generation</li>
             <li>Customer Support</li>
             <li>Unlimted Features</li>
         </ul>
-        <a href="https://buy.stripe.com/test_8wM2btacxcba8OA9AB" className="w-1/2 bg-primary text-white rounded-lg h-10 flex items-center justify-center">Click Here</a>
+        <a href="https://buy.stripe.com/test_8wM2btacxcba8OA9AB" className="w-1/2 bg-white text-primary rounded-lg h-10 flex items-center justify-center">Click Here</a>
     </div>
-    <div className="bg-background w-2/3 h-full rounded-lg p-4 flex flex-col justify-center items-center gap-4">
+    <div className="border border-primary w-2/3 h-full rounded-lg p-4 flex flex-col justify-center items-center gap-4">
         <h1 className="text-3xl font-bold justify-self-center">Yearly</h1>
         <h1 className="text-4xl font-bold text-primary">$30<span className="text-sm text-black font-normal">/year</span></h1>
         <ul className="text-center list-disc">
