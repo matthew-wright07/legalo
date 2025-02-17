cd5580aa3cefb7ce6186fa8febe2999c77bb0f82
Matthew <matthewjwright07@gmail.com>
2025-02-16 18:22:04 -0600

    Changed Some Text

--- components/home/Intro.js
+++ components/home/Intro.js
@@ -5,8 +5,8 @@ export default function Intro(){
             <h1 className="text-6xl font-bold text-primary">All In One Legal Tool</h1>
             <p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie facilisis velit. Duis non tellus blandit, auctor elit sit amet, pretium odio. Sed rhoncus, orci vel tincidunt congue, dui nulla ultrices velit, at ultricies ex est eu nunc. Nunc in dictum dolor, quis porttitor odio. In id lacus turpis.</p>
             <div className="flex items-center gap-4">
-            <a href="" className="h-12 w-32 bg-primary text-white flex justify-center items-center rounded-lg hover:bg-hover">Learn More</a>
-            <a href="" className="h-12 w-32 border border-primary text-primary flex justify-center items-center rounded-lg">Explore</a>
+            <a href="" className="h-12 w-32 bg-primary text-white flex justify-center items-center rounded-lg hover:bg-hover">Start</a>
+            <a href="" className="h-12 w-32 border border-primary text-primary flex justify-center items-center rounded-lg">Learn More</a>
             </div>
         </div>
         <div>
