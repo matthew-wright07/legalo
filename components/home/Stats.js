257a9a7a535bd779a35348295956de3ef71bbd17
Matthew <matthewjwright07@gmail.com>
2025-02-20 15:22:03 -0600

    Changed Some Text

--- components/home/Stats.js
+++ components/home/Stats.js
@@ -4,7 +4,7 @@ export default function Stats(){
             <h1 className="text-4xl font-bold">Simplify Your Legal Docs</h1>
             <div className="flex justify-between w-3/4 gap-8">
                 <h1 className="text-5xl text-primary text-center">70%<p className="text-2xl">Of Startups Struggle With Legal Paperwork</p></h1>
-                <h1 className="text-5xl text-primary text-center">30 Million<p className="text-2xl">Businesses In The U.S. Need Legal Documents</p></h1>
+                <h1 className="text-5xl text-primary text-center">30 Million<p className="text-2xl">Businesses In The USA Need Legal Documents</p></h1>
                 <h1 className="text-5xl text-primary text-center">50%<p className="text-2xl">Of Contracts Are Now Created Online</p></h1>
             </div>
         </div>
