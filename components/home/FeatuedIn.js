132382f9edc8516c146c2e50e450b322c1023427
Matthew <matthewjwright07@gmail.com>
2025-02-22 18:53:11 -0600

    Changed Some Photos

--- components/home/FeatuedIn.js
+++ components/home/FeatuedIn.js
@@ -3,8 +3,8 @@ export default function FeaturedIN(){
         <div className="flex flex-col items-center justify-center bg-background px-24 py-12 gap-4">
                 <h1 className="text-2xl font-bold">Featured In</h1>
                 <div className="flex items-center justify-center gap-24">
+                    <img src="/discord.png" className="h-10 grayscale"/>
                     <img src="/reddit.png" className="h-12 grayscale"/>
-                    <img src="/vercel.png" className="h-10 grayscale"/>
                     <img src="/producthunt.png" className="h-12 grayscale"/>
                 </div>
         </div>
