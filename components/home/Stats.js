0d786a91430ee7e5dc6246a204c90334cb8409c1
Matthew <matthewjwright07@gmail.com>
2025-02-24 21:30:45 -0600

    Changed Some Styling

--- components/home/Stats.js
+++ components/home/Stats.js
@@ -3,9 +3,9 @@ export default function Stats(){
         <div className="flex flex-col gap-12 px-24 justify-center items-center py-16 bg-background">
             <h1 className="text-4xl font-bold">Simplify Your Legal Docs</h1>
             <div className="flex justify-between w-3/4 gap-8">
-                <h1 className="text-5xl text-primary text-center">70%<p className="text-2xl">Of Startups Struggle With Legal Paperwork</p></h1>
-                <h1 className="text-5xl text-primary text-center">30 Million<p className="text-2xl">Businesses In The USA Need Legal Documents</p></h1>
-                <h1 className="text-5xl text-primary text-center">50%<p className="text-2xl">Of Contracts Are Now Created Online</p></h1>
+                <h1 className="text-5xl text-primary text-center"><span className="font-bold text-4xl">70%</span><p className="text-2xl">Of Startups Struggle With Legal Paperwork</p></h1>
+                <h1 className="text-5xl text-primary text-center"><span className="font-bold text-4xl">30 Million</span><p className="text-2xl">Businesses In The USA Need Legal Documents</p></h1>
+                <h1 className="text-5xl text-primary text-center"><span className="font-bold text-4xl">50%</span><p className="text-2xl">Of Contracts Are Now Created Online</p></h1>
             </div>
         </div>
     )
